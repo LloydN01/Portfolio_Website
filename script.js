e1ff2c468f6c87aa8ef6145f22610e5b0f010cf7
LloydN01 <lloydna0527@gmail.com>
2022-01-22 16:28:08 +0800

    Added a carousel

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-const test = () => {
-    console.log("Hello, World!");
+const images = [
+    "0.jpg",
+    "1.jpg",
+    "2.jpg",
+    "3.jpg"
+];
+
+let index = 0;
+
+const backArrow = () => {
+    if(index === 0){
+        index = images.length - 1;
+    } else{
+        index -= 1;
+    }
+    document.getElementById("placeholderImg").src = images[index];
+}
+
+const nextArrow = () => {
+    if(index === images.length - 1){
+        index = 0;
+    } else{
+        index += 1;
+    }
+    document.getElementById("placeholderImg").src = images[index];
 }
